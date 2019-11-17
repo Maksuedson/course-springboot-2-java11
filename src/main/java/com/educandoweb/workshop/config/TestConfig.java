34ee6f6410ae227a10e25a2bb8c9947a816b5176
Maksuedson <maksuedson@hotmail.com>
2019-11-16 22:12:49 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/workshop/config/TestConfig.java
+++ src/main/java/com/educandoweb/workshop/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.workshop.entities.Category;
 import com.educandoweb.workshop.entities.Order;
 import com.educandoweb.workshop.entities.OrderItem;
+import com.educandoweb.workshop.entities.Payment;
 import com.educandoweb.workshop.entities.Product;
 import com.educandoweb.workshop.entities.User;
 import com.educandoweb.workshop.enums.OrderStatus;
@@ -75,8 +76,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
+		
 	}
-	
-	
-
 }
--- src/main/java/com/educandoweb/workshop/entities/Order.java
+++ src/main/java/com/educandoweb/workshop/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.educandoweb.workshop.enums.OrderStatus;
@@ -37,6 +39,9 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 	}
@@ -79,6 +84,18 @@ public class Order implements Serializable {
 		}
 	}
 
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems(){
+		return items;
+	}	
+
 	public void setClient(User client) {
 		this.client = client;
 	}
@@ -89,11 +106,7 @@ public class Order implements Serializable {
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
-	}
-	
-	public Set<OrderItem> getItems(){
-		return items;
-	}
+	}	
 
 	@Override
 	public boolean equals(Object obj) {
