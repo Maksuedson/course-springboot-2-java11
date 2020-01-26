a70095139f6f79c0195e23ee3555c33952027dd3
Maksuedson <maksuedson@hotmail.com>
2020-01-26 19:45:32 -0300

    Tratamento de exceção - delete

--- src/main/java/com/educandoweb/workshop/services/UserService.java
+++ src/main/java/com/educandoweb/workshop/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.workshop.entities.User;
 import com.educandoweb.workshop.repositories.UserRepository;
+import com.educandoweb.workshop.services.exceptions.DatabaseException;
 import com.educandoweb.workshop.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
